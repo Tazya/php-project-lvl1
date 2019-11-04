cacc3cc43464689838ccee025af9444712d776df
Tazya <vengador32@gmail.com>
2019-11-04 16:37:44 +0600

    added even-game logic

--- src/Cli.php
+++ src/Cli.php
@@ -4,10 +4,14 @@ namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\Game\startGame;
 
 function run()
 {
     line('Welcome to the Brain Game!');
+    line('Answer "yes" if the number is even, othervise answer "no"');
+
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    startGame($name);
 }
